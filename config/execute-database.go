a1431d70418f04d5455e276683b572becbb2e0fc
ombima56 <ombimahillary6@gmail.com>
2024-12-14 23:30:25 +0300

    feat: implement error handling

--- config/execute-database.go
+++ config/execute-database.go
@@ -10,13 +10,14 @@ import (
 func ExecuteSchema(db *sql.DB, schemaPath string) error {
 	schema, err := os.ReadFile(schemaPath)
 	if err != nil {
+		log.Printf("Error reading schema file from %s: %v", schemaPath, err)
 		return fmt.Errorf("error reading schema file: %v", err)
 	}
 
 	_, err = db.Exec(string(schema))
 	if err != nil {
+		log.Printf("Error executing schema: %v", err)
 		return fmt.Errorf("error executing schema: %v", err)
 	}
-	log.Println("Database schema executed successfully")
 	return nil
 }
