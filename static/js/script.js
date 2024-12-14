1d202df91b6fb52e71df84e744eacfd5fc83cdd2
rod31 <rodneyotieno27@gmail.com>
2024-12-14 14:59:29 +0300

    handle password visibility toggle

--- static/js/script.js
+++ static/js/script.js
@@ -59,7 +59,23 @@ document
   sessionStorage.setItem("isLoggedIn", "true");
   window.location.href = "/dashboard";
 });
+// Password visibility toggle
+document.querySelectorAll(".toggle-password").forEach((button) => {
+    button.addEventListener("click", function () {
+      const input = this.parentElement.querySelector("input");
+      const icon = this.querySelector("i");
 
+      if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+      } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+      }
+    });
+  });
 
 
   document.querySelectorAll(".feature-card").forEach((card) => {
